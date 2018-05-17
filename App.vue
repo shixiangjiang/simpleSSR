fb0ed6e90064be7a3085a9f96290995b8dd99ccd
jiangshixiang <2445050242@qq.com>
2018-05-17 09:13:21 +0800

    update: 初次上传代码

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h1>App.vue</h1>
+        <p>vue with vue </p>
+        <hr>
+        <foo></foo>
+        <bar></bar>
+    </div>
+</template>
+<style>
+    h1 {
+        color: red
+    }
+</style>
+<script>
+    import Foo from './components/Foo.vue';
+    import Bar from './components/Bar.vue';
+
+    export default {
+        beforeCreate() {
+            console.log('App beforeCreate');
+        },
+        beforeMount() {
+            console.log('App beforeMount');
+        },
+        mounted() {
+            console.log('App mounted');
+        },
+        components: {
+            Foo,
+            Bar
+        }
+    }
+</script>
